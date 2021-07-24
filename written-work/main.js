408552355d78886dabe18c4a6c880c30667871e7
ashbob16478 <ashbob16478@gmail.com>
2021-07-24 19:00:26 +0100

    Synopsis Script Created

--- written-work/main.js
+++ written-work/main.js
@@ -1,15 +1,23 @@
 /* Blank JavaScript File */
 
 
-var synopsisBool;
-var synopsis;
-var holders;
-var mainContainer;
+var synopsisBool = false;
+var synopsis = null;
+var prevSynIndex = 0;
+var prevSynType = "none";
+
+var synJSON = null;
+
+var holders = null;
+var mainContainer = null;
 
 
 window.addEventListener("load", init, true); function init() {
     synopsisBool = false;
-    synopsis = document.getElementById("scriptSynopsis");
+    synopsis = document.getElementsByClassName("synopsis");
+    synJSON = "synopses.json";
+
+
     holders = document.getElementsByClassName("holder-ver");
 
     mainContainer = document.querySelector("#main-container");
@@ -18,37 +26,66 @@ window.addEventListener("load", init, true); function init() {
 function previewToggle(type, index) {
     if(synopsisBool == false) {
         showSynopsis(type, index);
+    } else if(index != prevSynIndex && type == prevSynType) {
+        showSynopsis(type, index);
+    } else if(type != prevSynType) {
+       hideSynopsis(prevSynType, prevSynIndex);
+       showSynopsis(type, index);
     } else {
-        hideSynopsis(type, index);
+         hideSynopsis(type, index);
     }
 }
 
 function showSynopsis(type, index) {
     synopsisBool = true;
-    synopsis.hidden = false
+    prevSynIndex = index;
+    prevSynType = type;
 
-    for (let i = 0; i < holders.length; i++) {
-        if(type == holders[i].id) {
-            mainContainer.style.height = "1100px";
+    if(synopsis != null) {
 
-            var holderEl = document.querySelector("#" + holders[i].id);
-            holderEl.style.height = "500px";
-            holderEl.style.maxHeight = "500px";
+        for (let i = 0; i < synopsis.length; i++) {
+            if(synopsis[i].id == type + "Syn") {
+                synopsis[i].hidden = false
+
+                fetch("synopses.json")
+                    .then(response => response.json())
+                    .then(data => {
+                        synopsis[i].innerText = data[0][type][index]
+                    })
+            }
+        }
+
+        for (let i = 0; i < holders.length; i++) {
+            if(type == holders[i].id) {
+                mainContainer.style.height = "1100px";
+
+                var holderEl = document.querySelector("#" + holders[i].id);
+                holderEl.style.height = "500px";
+                holderEl.style.maxHeight = "500px";
+            }
         }
     }
+    
 }
 
 function hideSynopsis(type, index) {
     synopsisBool = false;
-    synopsis.hidden = true;
 
-    for (let i = 0; i < holders.length; i++) {
-        if(type == holders[i].id) {
-            mainContainer.style.height = "800px";
+    if (synopsis != null) {
+        for (let i = 0; i < synopsis.length; i++) {
+            if(synopsis[i].id == type + "Syn") {
+                synopsis[i].hidden = true
+            }
+        }
 
-            var holderEl = document.querySelector("#" + holders[i].id);
-            holderEl.style.height = "230px";
-            holderEl.style.maxHeight = "230px";
+        for (let i = 0; i < holders.length; i++) {
+            if(type == holders[i].id) {
+                mainContainer.style.height = "800px";
+
+                var holderEl = document.querySelector("#" + holders[i].id);
+                holderEl.style.height = "230px";
+                holderEl.style.maxHeight = "230px";
+            }
         }
     }
 }
